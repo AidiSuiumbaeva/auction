8b8df25ae9b954aee710f3325f221c85c11bf91c
Aidai Suiumbaeva <aidisuiumbaeva@gmail.com>
2020-03-28 22:39:40 +0600

    initial commit

--- src/main/java/kg/megacom/auction/models/Lot.java
+++ src/main/java/kg/megacom/auction/models/Lot.java
@@ -1,4 +1,13 @@
 package kg.megacom.auction.models;
 
+import java.time.LocalDate;
+
 public class Lot {
+    private String name;
+    private double minCost;
+    private double price;
+    private int step;
+    private LocalDate startDate;
+    private LocalDate finishDate;
+
 }
